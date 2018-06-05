12c9d50ae9210db848f681c496f06e6d6898e79f
Junheng Wang <wangj14@rose-hulman.edu>
2018-06-05 18:41:45 -0400

    added second field, uses appendChild instead of  innerHTML

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const form = document.querySelector('form')
 
-const changeHeading = function(ev) {
+const addElements = function(ev) {
   ev.preventDefault()
 
   const f = ev.target
   const spellName = f.spellName.value
-
+  const spellEffect = f.spellEffect.value
   const spellsDiv = document.querySelector('#spells')
-  spellsDiv.innerHTML += `<li>${spellName}</li>`
+
+  var linode = document.createElement("LI");
+  var textnode = document.createTextNode(spellName + " :  " + spellEffect);
+  linode.appendChild(textnode);
+  spellsDiv.appendChild(linode);
 
   f.reset()
 }
 
-form.addEventListener('submit', changeHeading)
+form.addEventListener('submit', addElements)
