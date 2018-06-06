f9e19556161854c97a925ae001c47df725f7b34f
Junheng Wang <wangj14@rose-hulman.edu>
2018-06-05 20:10:15 -0400

    display name and effect in separate elements and style them

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const form = document.querySelector('form')
 
+const buildli = function() {
+  return document.createElement("LI")
+}
+
+const buildspells = function(f) {
+  const spellName = f.spellName.value
+  const spells = document.querySelector('#spells')
+  return document.createTextNode(spellName)
+}
+
+const buildeffect = function(f) {
+  const spellEffect = f.spellEffect.value
+  const effect = document.querySelector('#effect')
+  return document.createTextNode(spellEffect)
+}
+
+
 const addElements = function(ev) {
   ev.preventDefault()
-
   const f = ev.target
-  const spellName = f.spellName.value
-  const spellEffect = f.spellEffect.value
-  const spellsDiv = document.querySelector('#spells')
 
-  var linode = document.createElement("LI");
-  var textnode = document.createTextNode(spellName + " :  " + spellEffect);
-  linode.appendChild(textnode);
-  spellsDiv.appendChild(linode);
+  var linode1 = buildli()
+  var textnode1 = buildspells(f)
+  linode1.appendChild(textnode1)
+  spells.appendChild(linode1)
+
+  var linode2 = buildli()
+  var textnode2 = buildeffect(f)
+  linode2.appendChild(textnode2)
+  effect.appendChild(textnode2)
 
   f.reset()
 }
