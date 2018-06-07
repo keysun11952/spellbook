f879f468e6baecae76686f17df5a38411391b171
Junheng Wang <wangj14@rose-hulman.edu>
2018-06-07 14:49:27 -0400

    add fav button

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const app = {
-  spells: [],
-
   init: function () {
+    this.spells = []
+    this.template = document.querySelector('.spell.template')
+
     const form = document.querySelector('form')
     form.addEventListener('submit', (ev) => {
       ev.preventDefault()
@@ -18,36 +19,47 @@ const app = {
   },
 
   renderItem: function (spell) {
+    const item = this.template.cloneNode(true)
+    item.classList.remove('template')
+
     // ['name', 'level']
     const properties = Object.keys(spell)
 
-    // collect an array of <span> elements
-    const childElements = properties.map((prop) => {
-      return this.renderProperty(prop, spell[prop])
+    properties.forEach(property => {
+      const el = item.querySelector(`.${property}`)
+      el.textContent = spell[property]
+      el.setAttribute('title', spell[property])
     })
 
-    const item = document.createElement('li')
-    item.classList.add('spell')
+    // delete button
+    item
+      .querySelector('button.delete')
+      .addEventListener(
+        'click',
+        this.removeSpell.bind(this, spell)
+      )
 
-    const button = document.createElement('button')
-    button.classList.add('remove')
-    button.textContent = 'remove'
-    button.title = 'remove'
-
-    // append each <span> to the <li>
-    childElements.forEach(function (el) {
-      item.appendChild(el)
-      item.appendChild(button)
-    })
-
-    button.onclick = () => {
-      const toRemove = button.parentElement
-      toRemove.parentElement.removeChild(toRemove)
-      const i = this.spells.indexOf(spell)
-      this.spells.splice(i, 1)
-    }
+    // fav button
+    item.querySelector('button.fav').addEventListener('click', this.highlight.bind(this, spell))
 
     return item
+  },
+
+  highlight: function (spell, ev) {
+    const button = ev.target
+    const item = button.parentNode
+    item.querySelector('span').classList.add('fav')
+  },
+
+  removeSpell: function (spell, ev) {
+    // Remove from the DOM
+    const button = ev.target
+    const item = button.closest('.spell')
+    item.parentNode.removeChild(item)
+
+    // Remove from the array
+    const i = this.spells.indexOf(spell)
+    this.spells.splice(i, 1)
   },
 
   handleSubmit: function (ev) {
@@ -66,6 +78,7 @@ const app = {
     list.appendChild(item)
 
     f.reset()
+    f.spellName.focus()
   },
 }
 
